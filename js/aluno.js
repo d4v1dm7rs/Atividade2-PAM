7efb1f4cb5fc8ddf1a07d4935756e6a50a64a545
d4v1dm7rs <ds093461@gmail.com>
2025-04-04 22:35:27 -0300

    Continuando a atividade solicitada

--- js/aluno.js
+++ js/aluno.js
@@ -1,15 +1,9 @@
-namespace escola {
-    export class Aluno extends Pessoa {
-        private _matricula: number;
-        private _curso: string;
-        private _escola: string;
-        private _nota1: number;
-        private _nota2: number;
-        private _nota3: number;
-        private _nota4: number;
-
-        constructor(matricula: number, curso: string, escola: string, nota1: number, nota2: number, nota3: number, nota4: number) {
-            super ();
+"use strict";
+var escola;
+(function (escola_1) {
+    class Aluno extends escola_1.Pessoa {
+        constructor(matricula, curso, escola, nota1, nota2, nota3, nota4) {
+            super();
             this._matricula = matricula;
             this._curso = curso;
             this._escola = escola;
@@ -18,54 +12,48 @@ namespace escola {
             this._nota3 = nota3;
             this._nota4 = nota4;
         }
-
-        get matricula(){
+        get matricula() {
             return this._matricula;
         }
-        set matricula(matricula: number){
+        set matricula(matricula) {
             this._matricula = matricula;
         }
-
-        get curso(){
+        get curso() {
             return this._curso;
         }
-        set curso(curso: string){
+        set curso(curso) {
             this._curso = curso;
         }
-
-        get escola(){
+        get escola() {
             return this._escola;
         }
-        set escola(escola: string){
+        set escola(escola) {
             this._escola = escola;
         }
-
-        get nota1(){
+        get nota1() {
             return this._nota1;
         }
-        set nota1(nota1: number){
+        set nota1(nota1) {
             this._nota1 = nota1;
         }
-
-        get nota2(){
+        get nota2() {
             return this._nota2;
         }
-        set nota2(nota2: number){
+        set nota2(nota2) {
             this._nota2 = nota2;
         }
-
-        get nota3(){
+        get nota3() {
             return this._nota3;
         }
-        set nota3(nota3: number){
+        set nota3(nota3) {
             this._nota3 = nota3;
         }
-
-        get nota4(){
+        get nota4() {
             return this._nota4;
         }
-        set nota4(nota4: number){
+        set nota4(nota4) {
             this._nota4 = nota4;
         }
     }
-}
+    escola_1.Aluno = Aluno;
+})(escola || (escola = {}));
